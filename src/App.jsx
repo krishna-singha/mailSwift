898639b258be0a71ac0674578a3ab761c221692a
Rahul <chattychidiya@gmail.com>
2024-05-06 12:07:12 +0530

    setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,28 @@ import {
 	createRoutesFromChildren,
 	Navigate,
 } from "react-router-dom";
-import Navbar from "./components/Navigation/Navbar";
-import Component from "./components/Test";
 import Layout from "./Layout";
 import Subscriber from "./components/Subscribers/Subscriber";
 
 const router = createBrowserRouter(
 	createRoutesFromChildren(
 		<Route path="/" element={<Layout />}>
-			<Route path="" element={<Navigate to={"/subscriber"} />} />
-			<Route path="subscriber" element={<Subscriber />} />
-			<Route path="help" element={<div>help</div>} />
-			<Route path="about" element={<div>about</div>} />
+			<Route path="/" element={<Navigate to={"/dashboard"} />} />
+			<Route path="/dashboard" element={<div>Dashboard content</div>} />
+			<Route path="/campaigns" element={<div>Campaigns content</div>} />
+			<Route path="/templates" element={<div>Templates content</div>} />
+			<Route path="/subscribers" element={<Subscriber />} />
+			<Route path="/messages" element={<div>Messages content</div>} />
+			<Route
+				path="/email-services"
+				element={<div>Email Services content</div>}
+			/>
+			<Route
+				path="/manage-users"
+				element={<div>Manage Users content</div>}
+			/>
+			<Route path="/help" element={<div>Help content</div>} />
+			<Route path="/about" element={<div>About content</div>} />
 		</Route>
 	)
 );
